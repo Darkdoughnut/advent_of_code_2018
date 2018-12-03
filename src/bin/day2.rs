1b3a341cccf99beda2f2a505530c79b2dd006fa6
Justin Hao <djhao@umich.edu>
2018-12-02 21:02:36 -0800

    Finished day 2 part 2

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -1,7 +1,7 @@
 extern crate radix_trie;
 
 // use radix_trie::{Trie, TrieCommon};
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::string::String;
@@ -26,11 +26,14 @@ fn main() {
     // Part 2
     let file = File::open("input/day2").expect("Unable to open");
     let reader = BufReader::new(file);
+    find_similar_words(reader);
 }
 
 fn scan_word_for_n(word: &str, count: usize) -> i64 {
     let mut letter_map: HashMap<char, usize> = HashMap::new();
-
+    for c in word.chars() {
+        *(letter_map.entry(c).or_insert(0)) += 1;
+    }
     for (_letter, letter_count) in letter_map {
         if letter_count == count {
             return 1;
@@ -42,8 +45,56 @@ fn scan_word_for_n(word: &str, count: usize) -> i64 {
 fn find_similar_words<B: BufRead>(reader: B) -> String {
     // When inserting a word in the trie, a \n character
     // will signify the end of the word
-    let mut word_bank: HashSet<String> = HashSet::new();
+    let mut word_bank: Vec<String> = Vec::new();
+    let mut package_ids = Vec::new();
+    for line in reader.lines() {
+        package_ids.push(line.unwrap());
+    }
+    for line in package_ids {
+        let mut found_word = false;
+        for word in &word_bank {
+            if is_correct_hamming_distance(word.clone(), &line.clone()) {
+                println!("{:?}", word);
+                found_word = true;
+            }
+        }
+        word_bank.push(line);
+    }
     return "Hi".to_string();
+}
+
+fn is_correct_hamming_distance(first: String, second: &String) -> bool {
+    // Check length is the same
+    if first.len() != second.len() {
+        return false;
+    }
+    //Iterate and find total diff in chars
+    let mut first_itr = first.chars();
+    let mut second_itr = second.chars();
+    let mut diff_found = false;
+    loop {
+        match first_itr.next() {
+            Some(x) => {
+                let y = second_itr.next().unwrap();
+                if x != y {
+                    if diff_found {
+                        return false;
+                    } else {
+                        diff_found = true;
+                    }
+                }
+            }
+            None => {
+                if diff_found {
+                    println!("First word:  {:?}", first);
+                    println!("Second word: {:?}", second);
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
 }
 
 // TODO: FIGURE OUT TRIE IMPL
