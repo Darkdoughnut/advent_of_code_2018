25ebc5bb5d5e309c862cff94057791f9a3b7afcb
Justin Hao <djhao@umich.edu>
2018-12-02 22:06:51 -0800

    Finished day 2 part 2. Modified a function

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -42,7 +42,7 @@ fn scan_word_for_n(word: &str, count: usize) -> i64 {
     return 0;
 }
 
-fn find_similar_words<B: BufRead>(reader: B) -> String {
+fn find_similar_words<B: BufRead>(reader: B) -> () {
     // When inserting a word in the trie, a \n character
     // will signify the end of the word
     let mut word_bank: Vec<String> = Vec::new();
@@ -60,7 +60,7 @@ fn find_similar_words<B: BufRead>(reader: B) -> String {
         }
         word_bank.push(line);
     }
-    return "Hi".to_string();
+    return;
 }
 
 fn is_correct_hamming_distance(first: String, second: &String) -> bool {
