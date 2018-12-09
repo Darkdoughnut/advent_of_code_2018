12d63085243e379b2ece29e2a65641dcd5d99e4c
Justin Hao <djhao@umich.edu>
2018-12-08 22:27:26 -0800

    :sparkles::sparkles: Finished day 5 part 2 and part 1

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -1,6 +1,11 @@
 use std::fs::File;
 use std::io::Read;
 
+static ASCII_LOWER: [char; 26] = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z',
+];
+
 fn is_reaction_pair(a: char, b: char) -> bool {
     assert!(a.is_lowercase() || a.is_uppercase());
     assert!(b.is_lowercase() || b.is_uppercase());
@@ -25,18 +30,52 @@ fn main() {
         .expect("Unable to read the file");
     let mut reaction_vector: Vec<char> = Vec::new();
     for c in contents.chars() {
-        let end = reaction_vector.last().clone();
-        match end {
-            Some(x) => {
-                if is_reaction_pair(*x, c) {
-                    reaction_vector.pop();
+        let mut react = false;
+        {
+            let end = reaction_vector.last().clone();
+            match end {
+                Some(x) => {
+                    react = is_reaction_pair(*x, c);
                 }
+                None => {}
             }
-            None => reaction_vector.push(c),
+        }
+        if react {
+            reaction_vector.pop();
+        } else {
+            reaction_vector.push(c);
         }
     }
     println!("Length after reaction: {}", reaction_vector.len());
     println!("----- Part 2 -----");
+    let mut best_reaction_size = reaction_vector.len();
+    for letter in &ASCII_LOWER {
+        let mut vec_copy: Vec<char> = Vec::new();
+        for c in reaction_vector.clone() {
+            if c.to_lowercase().to_string() == letter.to_string() {
+                continue;
+            }
+            let mut react = false;
+            {
+                let last_elem = vec_copy.last();
+                match last_elem {
+                    Some(x) => {
+                        react = is_reaction_pair(*x, c);
+                    }
+                    None => {}
+                }
+            }
+            if react {
+                vec_copy.pop();
+            } else {
+                vec_copy.push(c);
+            }
+        }
+        if vec_copy.len() < best_reaction_size {
+            best_reaction_size = vec_copy.len();
+        }
+    }
+    println!("Smallest reaction size: {}", best_reaction_size)
 }
 
 // #[test]
