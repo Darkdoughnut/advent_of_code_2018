1b42b740932adfc4da4e5e6ea45e536ec8602121
Justin Hao <djhao@umich.edu>
2018-12-08 20:57:25 -0800

    :construction: Working on fixing day5

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -1,41 +1,42 @@
-use std::io::{BufRead, BufReader};
 use std::fs::File;
+use std::io::Read;
 
 fn is_reaction_pair(a: char, b: char) -> bool {
     assert!(a.is_lowercase() || a.is_uppercase());
     assert!(b.is_lowercase() || b.is_uppercase());
 
-    if (a.is_lowercase() != b.is_lowercase()){ // xor
-        if (a.to_lowercase() == b.to_lowercase()){
+    if a.is_lowercase() != b.is_lowercase() {
+        // xor
+        if a.to_lowercase().to_string() == b.to_lowercase().to_string() {
             return true;
-        }else{
+        } else {
             return false;
         }
-    }else{
+    } else {
         return false;
     }
 }
 
 fn main() {
     println!("----- Part 1 -----");
-    let file = File::open("input/day5");
-    let reader = BufReader::new(file);
+    let mut file = File::open("input/day5").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("Unable to read the file");
     let mut reaction_vector: Vec<char> = Vec::new();
-    for c in reader.chars() {
-        let end = reaction_vector.last();
+    for c in contents.chars() {
+        let end = reaction_vector.last().clone();
         match end {
             Some(x) => {
-                if is_reaction_pair(x, c){
+                if is_reaction_pair(*x, c) {
                     reaction_vector.pop();
                 }
-            },
-            None => reaction_vector.push(c);
+            }
+            None => reaction_vector.push(c),
         }
-
     }
     println!("Length after reaction: {}", reaction_vector.len());
     println!("----- Part 2 -----");
 }
 
-#[test]
-
+// #[test]
